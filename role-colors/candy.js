fd97f6f5822450f5a01b715c1df4b1bc4af849fe
Joshua Heiks <joshauheiks@gmail.com>
2014-03-07 21:55:15 -0500

    Added error checking to fix part of issue #1. The plugin should now be more stable.

--- role-colors/candy.js
+++ role-colors/candy.js
@@ -1,26 +1,31 @@
 /*
- *
- *
+ * RoleColors:
+ * Changes the style of messages based on users role and affiliation.
  */
-
 
 var CandyShop = (function(self) { return self; }(CandyShop || {}));
 
 CandyShop.RoleColors = (function(self, Candy, $) {
     self.init = function() {
-        
         $(Candy).on('candy:view.message.after-show', changeMessageRoleColors);
-        
         return self;
     };
     
     function changeMessageRoleColors(e, args) {
-        var senderJid = args.roomJid + '/' + args.name;
-        var sender = Candy.Core.getRoom(args.roomJid).roster.items[senderJid];
-        var role = sender.getRole();
-        var affiliation = sender.getAffiliation();
-        args.element.addClass('rolecolors-role-' + role);
-        args.element.addClass('rolecolors-affiliation-' + affiliation);
+        try {
+            var senderJid = args.roomJid + '/' + args.name;
+            var room = Candy.Core.getRoom(args.roomJid);
+            if (!room) return;
+            var roster = room.roster;
+            if (!roster) return;
+            var sender = roster.items[senderJid];
+            var role = sender.getRole();
+            var affiliation = sender.getAffiliation();
+            args.element.addClass('rolecolors-role-' + role);
+            args.element.addClass('rolecolors-affiliation-' + affiliation);
+        } catch(er) {
+            Candy.Core.log('[Plugin Error: RoleColors]');
+        }
     }
     return self;
 
